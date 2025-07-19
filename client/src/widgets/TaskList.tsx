b2414b608883b44a140e977b0785ae8083a795fd
walle <walllle.ru@gmail.com>
2025-07-19 21:22:55 +0500

    add prettier support

--- client/src/widgets/TaskList.tsx
+++ client/src/widgets/TaskList.tsx
@@ -2,14 +2,14 @@ import type { Task } from '@/entities/task/model/Task'
 import TaskItem from '../entities/task/ui/TaskItem'
 
 interface TaskListProps {
-  tasks: Task[];
+    tasks: Task[];
 }
 
 export default function TaskList({ tasks }: TaskListProps) {
-  const listTasks = tasks.map(task => <TaskItem key={task.id} {...task} />)
-  return (
-    <div className='grid md:grid-cols-2 gap-5 lg:grid-cols-3 m-5'>
-      {listTasks}
-    </div>
-  )
+    const listTasks = tasks.map(task => <TaskItem key={task.id} {...task} />)
+    return (
+        <div className='grid md:grid-cols-2 gap-5 lg:grid-cols-3 m-5'>
+            {listTasks}
+        </div>
+    )
 }
