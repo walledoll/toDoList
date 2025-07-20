3f93bf6dd20c8929b37ac21a6229c3766fe3721b
walle <walllle.ru@gmail.com>
2025-07-20 20:39:36 +0500

    fix cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,10 +4,9 @@ import cors from 'cors';
 
 const app = express();
 const port = 3000;
-
+app.use(cors({ origin: 'https://todolist-2qqh.onrender.com ' })); 
 app.use(express.json());
 app.use('/', taskRoutes);
-app.use(cors());
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
