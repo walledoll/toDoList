9cc3686f4f4e9c53092bb2b70d82876e7fc468e9
walle <walllle.ru@gmail.com>
2025-07-21 02:39:28 +0500

    fix: add minimal form submit

--- client/src/pages/New.tsx
+++ client/src/pages/New.tsx
@@ -28,6 +28,10 @@ export default function CreateTaskPage() {
 
     const handleSubmit = () => {
         if (currentTask) {
+            if(!currentTask.name || currentTask.content) {
+                alert('Пожалуйста, заполните все обязательные поля');
+                return;
+            }          
             createTask.mutate(currentTask, {
                 onSuccess: () => {
                     navigate('/');
