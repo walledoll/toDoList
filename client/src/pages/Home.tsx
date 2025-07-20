7c7c5be2dff1f3a995cac301d79091cf40af15de
walle <walllle.ru@gmail.com>
2025-07-20 21:02:40 +0500

    fixes

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -7,7 +7,8 @@ import { useTasks } from '@/app/hooks/useTasks';
 export default function Home() {
     const { data: tasks, isLoading, error } = useTasks();
     const navigate = useNavigate();
-    if (error || !tasks) return <div>Ошибка загрузки задач</div>;
+    if( !tasks) return <div>Нет задач</div>
+    if (error ) return <div>Ошибка загрузки задач</div>;
     if (isLoading) return <div>Загрузка задач...</div>;
 
     const handleAddTask = () => {
